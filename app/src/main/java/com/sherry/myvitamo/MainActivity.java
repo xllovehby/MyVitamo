6ccb60ace265a244fa3885148fdbeaf963484c93
sherry <xl123789@foxmail.com>
2015-07-17 11:12:15 +0800

    1、用vitamo播放视频，可以手势控制音量和亮度大小，且视频大小可以双击放大缩小，很不错的。

---
+++ app/src/main/java/com/sherry/myvitamo/MainActivity.java
@@ -0,0 +1,245 @@
+package com.sherry.myvitamo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.media.AudioManager;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import io.vov.vitamio.widget.MediaController;
+import io.vov.vitamio.widget.VideoView;
+
+/**
+ *
+ * Android万能播放器
+ */
+public class MainActivity extends Activity {
+ /* sd卡里面的视频路径*/
+    private String path = Environment.getExternalStorageDirectory()
+            + "/videoDB2.FLV";
+    private VideoView mVideoView;
+  /*  音量和亮度调节*/
+    private View mVolumeBrightnessLayout;
+    private ImageView mOperationBg;
+    private ImageView mOperationPercent;
+    /*音频控制*/
+    private AudioManager mAudioManager;
+    /** 最大声音 */
+    private int mMaxVolume;
+    /** 当前声音 */
+    private int mVolume = -1;
+    /** 当前亮度 */
+    private float mBrightness = -1f;
+    /** 当前缩放模式 */
+    private int mLayout = VideoView.VIDEO_LAYOUT_ZOOM;
+    /**手势检测*/
+    private GestureDetector mGestureDetector;
+    private MediaController mMediaController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //隐藏导航栏
+        hideNavigationBar();
+        super.onCreate(savedInstanceState);
+        /*自定义*/
+        if (!io.vov.vitamio.LibsChecker.checkVitamioLibs(this))
+            return;
+        setContentView(R.layout.activity_main);
+        /*初始化*/
+        mVideoView = (VideoView) findViewById(R.id.surface_view);
+        mVolumeBrightnessLayout = findViewById(R.id.operation_volume_brightness);
+        mOperationBg = (ImageView) findViewById(R.id.operation_bg);
+        mOperationPercent = (ImageView) findViewById(R.id.operation_percent);
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        mMaxVolume = mAudioManager
+                .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        mVideoView.setVideoPath(path);
+        mMediaController = new MediaController(this);
+        mVideoView.setMediaController(mMediaController);
+        mVideoView.requestFocus();
+
+        mGestureDetector = new GestureDetector(this, new MyGestureListener());
+    }
+
+    //隐藏导航栏
+    public void hideNavigationBar() {
+//		int uiFlags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION ;// hide nav bar
+
+        int uiFlags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION// hide nav bar
+                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                   | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN; // hide status bar
+
+        if (android.os.Build.VERSION.SDK_INT >= 19) {
+            uiFlags |= 0x00001000; // SYSTEM_UI_FLAG_IMMERSIVE_STICKY: hide
+            // navigation bars - compatibility: building
+            // API level is lower thatn 19, use magic
+            // number directly for higher API target
+            // level
+        } else {
+            uiFlags |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+        }
+
+        getWindow().getDecorView().setSystemUiVisibility(uiFlags);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideNavigationBar();
+        }
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (mGestureDetector.onTouchEvent(event))
+            return true;
+
+        // 处理手势结束
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+                endGesture();
+                break;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    /** 手势结束 */
+    private void endGesture() {
+        mVolume = -1;
+        mBrightness = -1f;
+
+        // 隐藏
+        mDismissHandler.removeMessages(0);
+        mDismissHandler.sendEmptyMessageDelayed(0, 500);
+    }
+
+    private class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        /** 双击 */
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            if (mLayout == VideoView.VIDEO_LAYOUT_ZOOM)
+                mLayout = VideoView.VIDEO_LAYOUT_ORIGIN;
+            else
+                mLayout++;
+            if (mVideoView != null)
+                mVideoView.setVideoLayout(mLayout, 0);
+            return true;
+        }
+
+        /** 滑动 */
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                float distanceX, float distanceY) {
+            float mOldX = e1.getX(), mOldY = e1.getY();
+            int y = (int) e2.getRawY();
+            Display disp = getWindowManager().getDefaultDisplay();
+            int windowWidth = disp.getWidth();
+            int windowHeight = disp.getHeight();
+
+            if (mOldX > windowWidth * 4.0 / 5)// 右边滑动改变声音大小
+                onVolumeSlide((mOldY - y) / windowHeight);
+            else if (mOldX < windowWidth / 5.0)// 左边滑动改变亮度大小
+                onBrightnessSlide((mOldY - y) / windowHeight);
+
+            return super.onScroll(e1, e2, distanceX, distanceY);
+        }
+    }
+
+    /** 定时隐藏 */
+    private Handler mDismissHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            mVolumeBrightnessLayout.setVisibility(View.GONE);
+        }
+    };
+
+    /**
+     * 滑动改变声音大小
+     *
+     * @param percent
+     */
+    private void onVolumeSlide(float percent) {
+        if (mVolume == -1) {
+            mVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            if (mVolume < 0)
+                mVolume = 0;
+
+            // 显示声音图片
+            mOperationBg.setImageResource(R.drawable.video_volumn_bg);
+            mVolumeBrightnessLayout.setVisibility(View.VISIBLE);
+        }
+
+        int index = (int) (percent * mMaxVolume) + mVolume;
+        if (index > mMaxVolume)
+            index = mMaxVolume;
+        else if (index < 0)
+            index = 0;
+
+        // 变更声音
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, index, 0);
+
+        // 变更进度条
+        ViewGroup.LayoutParams lp = mOperationPercent.getLayoutParams();
+        lp.width = findViewById(R.id.operation_full).getLayoutParams().width
+                * index / mMaxVolume;
+        mOperationPercent.setLayoutParams(lp);
+    }
+
+    /**
+     * 滑动改变亮度
+     *
+     * @param percent
+     */
+    private void onBrightnessSlide(float percent) {
+        if (mBrightness < 0) {
+            mBrightness = getWindow().getAttributes().screenBrightness;
+            if (mBrightness <= 0.00f)
+                mBrightness = 0.50f;
+            if (mBrightness < 0.01f)
+                mBrightness = 0.01f;
+
+            // 显示
+            mOperationBg.setImageResource(R.drawable.video_brightness_bg);
+            mVolumeBrightnessLayout.setVisibility(View.VISIBLE);
+        }
+        WindowManager.LayoutParams lpa = getWindow().getAttributes();
+        lpa.screenBrightness = mBrightness + percent;
+        if (lpa.screenBrightness > 1.0f)
+            lpa.screenBrightness = 1.0f;
+        else if (lpa.screenBrightness < 0.01f)
+            lpa.screenBrightness = 0.01f;
+        getWindow().setAttributes(lpa);
+
+        ViewGroup.LayoutParams lp = mOperationPercent.getLayoutParams();
+        lp.width = (int) (findViewById(R.id.operation_full).getLayoutParams().width * lpa.screenBrightness);
+        mOperationPercent.setLayoutParams(lp);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        if (mVideoView != null)
+            mVideoView.setVideoLayout(mLayout, 0);
+        super.onConfigurationChanged(newConfig);
+    }
+
+}
